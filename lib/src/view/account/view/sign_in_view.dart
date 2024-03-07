026edff83f1e1f850326e1e265e8754289cd9acc
Haejun Kim <109138345+haejun-kim@users.noreply.github.com>
2024-03-07 15:49:41 +0900

    feat: Add Signup page (#8)
    * feat: add widget SafeArea in SignInView
    * feat: add Sign Up view
    * add sign up page
    * add kakao login button

--- lib/src/view/account/view/sign_in_view.dart
+++ lib/src/view/account/view/sign_in_view.dart
@@ -1,3 +1,4 @@
+import 'package:cat_long_live/src/view/account/view/sign_up_view.dart';
 import 'package:cat_long_live/theme/component/button/button.dart';
 import 'package:cat_long_live/theme/component/button/kakao_button.dart';
 import 'package:cat_long_live/theme/component/hide_keyboard.dart';
@@ -12,97 +13,105 @@ class SignInView extends StatelessWidget {
   Widget build(BuildContext context) {
     return HideKeyboard(
       child: Scaffold(
-        body: SingleChildScrollView(
-          child: Center(
-            child: Column(
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(
-                      top: MediaQuery.of(context).size.height * 0.16),
-                  child: Text(
-                    "Sign In",
-                    style: context.theme.typo.headline1
-                        .copyWith(fontWeight: context.typo.bold, fontSize: 32),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  "Welcome back",
-                  style: context.typo.headline4.copyWith(
-                    color: context.color.subtext,
-                  ),
-                ),
-                const SizedBox(height: 50),
-                const InputField(
-                  hint: "이메일을 입력해주세요.",
-                  icon: "material-mail",
-                  labelText: "Email",
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                const InputField(
-                  hint: "비밀번호를 입력해주세요.",
-                  icon: "material-lock",
-                  labelText: "Password",
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                Align(
-                  alignment: const Alignment(0.87, 0),
-                  child: TextButton(
-                    onPressed: () {},
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Center(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(
+                        top: MediaQuery.of(context).size.height * 0.16),
                     child: Text(
-                      "Forgot password?",
-                      style: context.typo.subtitle2
-                          .copyWith(color: context.color.primary),
+                      "Sign In",
+                      style: context.theme.typo.headline1.copyWith(
+                          fontWeight: context.typo.bold, fontSize: 32),
                     ),
                   ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Button(
-                  onPressed: () {},
-                  text: "로그인",
-                  width: 183,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  "OR",
-                  style: context.typo.body1.copyWith(
-                    color: context.color.subtext,
-                  ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                const KakaoButton(),
-                const SizedBox(
-                  height: 30,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("아직 회원이 아니신가요?", style: context.typo.body1),
-                    const SizedBox(
-                      width: 30,
+                  const SizedBox(height: 10),
+                  Text(
+                    "Welcome back",
+                    style: context.typo.headline4.copyWith(
+                      color: context.color.subtext,
                     ),
-                    TextButton(
+                  ),
+                  const SizedBox(height: 50),
+                  const InputField(
+                    hint: "이메일을 입력해주세요.",
+                    icon: "material-mail",
+                    labelText: "Email",
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  const InputField(
+                    hint: "비밀번호를 입력해주세요.",
+                    icon: "material-lock",
+                    labelText: "Password",
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Align(
+                    alignment: const Alignment(0.87, 0),
+                    child: TextButton(
                       onPressed: () {},
                       child: Text(
-                        "회원가입",
-                        style: context.typo.body1.copyWith(
-                            color: context.color.primary,
-                            fontWeight: context.typo.bold),
+                        "Forgot password?",
+                        style: context.typo.subtitle2
+                            .copyWith(color: context.color.primary),
                       ),
-                    )
-                  ],
-                ),
-              ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Button(
+                    onPressed: () {},
+                    text: "로그인",
+                    width: 183,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    "OR",
+                    style: context.typo.body1.copyWith(
+                      color: context.color.subtext,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const KakaoButton(),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("아직 회원이 아니신가요?", style: context.typo.body1),
+                      const SizedBox(
+                        width: 30,
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.push(context, MaterialPageRoute(
+                            builder: (context) {
+                              return const SignUpView();
+                            },
+                          ));
+                        },
+                        child: Text(
+                          "회원가입",
+                          style: context.typo.body1.copyWith(
+                              color: context.color.primary,
+                              fontWeight: context.typo.bold),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/theme/component/button/kakao_button.dart
+++ lib/theme/component/button/kakao_button.dart
@@ -6,10 +6,7 @@ class KakaoButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TextButton(
-      onPressed: () {
-        // 버튼을 눌렀을 때 수행할 동작
-        print("kakao login button pressed!");
-      },
+      onPressed: () {},
       child: Image.asset(
         "assets/images/kakao_login_medium_narrow.png", // 이미지 파일 경로
         width: 183, // 이미지의 가로 크기
