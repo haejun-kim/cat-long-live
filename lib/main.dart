9f31174a5f074e5f98339eb7ec00731266c25982
haejun-kim <hj885353@gmail.com>
2024-03-14 20:20:25 +0900

    feat: connect localhost pocketbase and add dotenv for manage to environment variable

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,14 @@ import 'package:cat_long_live/src/service/health_service.dart';
 import 'package:cat_long_live/src/service/theme_service.dart';
 import 'package:cat_long_live/src/view/account/sign_in/sign_in_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
   // intl 패키지 초기화
   await initializeDateFormatting();
+  await dotenv.load(fileName: 'assets/config/.env');
 
   runApp(
     MultiProvider(
