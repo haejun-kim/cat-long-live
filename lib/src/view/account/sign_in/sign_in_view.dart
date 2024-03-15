6669d55b7da9f59a45b1867f3b0f2640160b5efe
haejun-kim <hj885353@gmail.com>
2024-03-15 19:43:26 +0900

    feat: kakao login using provided pocketbase dart sdk

--- lib/src/view/account/sign_in/sign_in_view.dart
+++ lib/src/view/account/sign_in/sign_in_view.dart
@@ -32,151 +32,163 @@ class _SignInViewState extends State<SignInView> {
     return HideKeyboard(
       child: BaseView(
         viewModel: signInViewModel,
-        builder: (context, viewModel) => Scaffold(
-          body: SafeArea(
-            child: SingleChildScrollView(
-              child: Center(
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(
-                          top: MediaQuery.of(context).size.height * 0.16),
-                      child: Text(
-                        "Sign In",
-                        style: context.theme.typo.headline1.copyWith(
-                            fontWeight: context.typo.bold, fontSize: 32),
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    Text(
-                      "Welcome back",
-                      style: context.typo.headline4.copyWith(
-                        color: context.color.subtext,
-                      ),
-                    ),
-                    const SizedBox(height: 50),
-                    InputField(
-                      hint: "이메일을 입력해주세요.",
-                      icon: "material-mail",
-                      labelText: "Email",
-                      maxLength: 30,
-                      horizontalPaddingSize: paddingSize,
-                      controller: viewModel.emailController,
-                      validator: (value) => Validator.validateEmail(
-                        viewModel.emailController.text,
-                      ),
-                      onChanged: (value) {
-                        viewModel.updateEmail(value);
-                      },
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    InputField(
-                      hint: "비밀번호를 입력해주세요.",
-                      icon: "material-lock",
-                      labelText: "Password",
-                      isObscure: true,
-                      horizontalPaddingSize: paddingSize,
-                      controller: viewModel.passwordController,
-                      validator: (value) => Validator.validatePassword(
-                        viewModel.passwordController.text,
-                      ),
-                      onChanged: (value) {
-                        viewModel.updatePassword(value);
-                      },
-                    ),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    Align(
-                      alignment: const Alignment(0.87, 0),
-                      child: TextButton(
-                        onPressed: () {},
-                        child: Text(
-                          "Forgot password?",
-                          style: context.typo.subtitle2
-                              .copyWith(color: context.color.primary),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-
-                    /// 로그인 버튼
-                    Button(
-                      onPressed: () async {
-                        try {
-                          await viewModel.accountRepository
-                              .signIn(viewModel.body);
-
-                          await Navigator.pushAndRemoveUntil(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const HomePageView(),
-                            ),
-                            (route) => false,
-                          );
-                        } catch (e) {
-                          print(e);
-                          await ToastUtils.showToast("등록되지 않은 사용자입니다.");
-                        } finally {
-                          setState(() {
-                            viewModel.emailController.clear();
-                            viewModel.passwordController.clear();
-                          });
-                        }
-                      },
-                      text: "로그인",
-                      width: 183,
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      "OR",
-                      style: context.typo.body1.copyWith(
-                        color: context.color.subtext,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    const KakaoButton(),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+        builder: (context, viewModel) =>
+            Scaffold(
+              body: SafeArea(
+                child: SingleChildScrollView(
+                  child: Center(
+                    child: Column(
                       children: [
-                        Text("아직 회원이 아니신가요?", style: context.typo.body1),
-                        const SizedBox(
-                          width: 30,
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            Navigator.push(context, MaterialPageRoute(
-                              builder: (context) {
-                                return const SignUpView();
-                              },
-                            ));
-                          },
+                        Padding(
+                          padding: EdgeInsets.only(
+                              top: MediaQuery
+                                  .of(context)
+                                  .size
+                                  .height * 0.16),
                           child: Text(
-                            "회원가입",
-                            style: context.typo.body1.copyWith(
-                                color: context.color.primary,
-                                fontWeight: context.typo.bold),
+                            "Sign In",
+                            style: context.theme.typo.headline1.copyWith(
+                                fontWeight: context.typo.bold, fontSize: 32),
                           ),
-                        )
+                        ),
+                        const SizedBox(height: 10),
+                        Text(
+                          "Welcome back",
+                          style: context.typo.headline4.copyWith(
+                            color: context.color.subtext,
+                          ),
+                        ),
+                        const SizedBox(height: 50),
+                        InputField(
+                          hint: "이메일을 입력해주세요.",
+                          icon: "material-mail",
+                          labelText: "Email",
+                          maxLength: 30,
+                          horizontalPaddingSize: paddingSize,
+                          controller: viewModel.emailController,
+                          validator: (value) =>
+                              Validator.validateEmail(
+                                viewModel.emailController.text,
+                              ),
+                          onChanged: (value) {
+                            viewModel.updateEmail(value);
+                          },
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        InputField(
+                          hint: "비밀번호를 입력해주세요.",
+                          icon: "material-lock",
+                          labelText: "Password",
+                          isObscure: true,
+                          horizontalPaddingSize: paddingSize,
+                          controller: viewModel.passwordController,
+                          validator: (value) =>
+                              Validator.validatePassword(
+                                viewModel.passwordController.text,
+                              ),
+                          onChanged: (value) {
+                            viewModel.updatePassword(value);
+                          },
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        Align(
+                          alignment: const Alignment(0.87, 0),
+                          child: TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              "Forgot password?",
+                              style: context.typo.subtitle2
+                                  .copyWith(color: context.color.primary),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+
+                        /// 로그인 버튼
+                        Button(
+                          onPressed: () async {
+                            try {
+                              await viewModel.accountRepository
+                                  .signIn(viewModel.body);
+
+                              await Navigator.pushAndRemoveUntil(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const HomePageView(),
+                                ),
+                                    (route) => false,
+                              );
+                            } catch (e) {
+                              print(e);
+                              await ToastUtils.showToast("등록되지 않은 사용자입니다.");
+                            } finally {
+                              setState(() {
+                                viewModel.emailController.clear();
+                                viewModel.passwordController.clear();
+                              });
+                            }
+                          },
+                          text: "로그인",
+                          width: 183,
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          "OR",
+                          style: context.typo.body1.copyWith(
+                            color: context.color.subtext,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        KakaoButton(onSuccess: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context)
+                          =>
+                          const HomePageView()
+                          ));
+                        }),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("아직 회원이 아니신가요?", style: context.typo.body1),
+                            const SizedBox(
+                              width: 30,
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.push(context, MaterialPageRoute(
+                                  builder: (context) {
+                                    return const SignUpView();
+                                  },
+                                ));
+                              },
+                              child: Text(
+                                "회원가입",
+                                style: context.typo.body1.copyWith(
+                                    color: context.color.primary,
+                                    fontWeight: context.typo.bold),
+                              ),
+                            )
+                          ],
+                        ),
                       ],
                     ),
-                  ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ),
       ),
     );
   }
--- lib/theme/component/kakao_button.dart
+++ lib/theme/component/kakao_button.dart
@@ -1,12 +1,46 @@
+import 'package:cat_long_live/src/service/pocketbase_service.dart';
+import 'package:cat_long_live/theme/component/toast.dart';
 import 'package:flutter/material.dart';
 
 class KakaoButton extends StatelessWidget {
-  const KakaoButton({super.key});
+  const KakaoButton({super.key, required this.onSuccess});
+
+  final Function onSuccess;
 
   @override
   Widget build(BuildContext context) {
     return TextButton(
-      onPressed: () {},
+      style: ButtonStyle(
+        overlayColor: MaterialStateProperty.resolveWith<Color>(
+            (Set<MaterialState> states) {
+          return Colors.transparent; // 클릭 했을 때의 overlay color를 투명으로 설정
+        }),
+        shadowColor: MaterialStateProperty.resolveWith<Color>(
+            (Set<MaterialState> states) {
+          return Colors.transparent; // 그림자 색상을 투명으로 설정
+        }),
+        elevation: MaterialStateProperty.resolveWith<double>(
+            (Set<MaterialState> states) {
+          return 0; // 그림자의 높이를 0으로 설정하여 그림자를 제거
+        }),
+      ),
+      onPressed: () async {
+        try {
+          final authData = await pb.collection('users').authWithOAuth2(
+            "kakao",
+            (url) async {
+              print("redirect url: $url");
+            },
+          );
+          onSuccess(); // Kakao 로그인 성공 후 실행할 코드를 호출합니다
+        } catch (e) {
+          ToastUtils.showToast(
+            "로그인에 실패했습니다. 다시 시도해주세요.",
+          );
+        }
+
+        /// TODO: authData를 Account 객체로 전달
+      },
       child: Image.asset(
         "assets/images/kakao_login_medium_narrow.png", // 이미지 파일 경로
         width: 183, // 이미지의 가로 크기
